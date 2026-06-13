28b1cbf470afab9a02b56781079ae4a55529e20e
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-06-13 22:20:28 +0700

    validate datatype

---
+++ projects/lesson4/lesson4.js
@@ -0,0 +1,11 @@
+const sum = (a, b) => {
+  return a + b;
+};
+console.log(sum(1, 2));
+
+//need to validate
+const sum1 = (x, y) => {
+  return x + y;
+};
+
+console.log(sum1("name", 2));
---
+++ projects/lesson4/lesson4.ts
@@ -0,0 +1,4 @@
+const sum3 = (a: number, b: number) => {
+  return a + b;
+};
+console.log(sum3(1, 2));

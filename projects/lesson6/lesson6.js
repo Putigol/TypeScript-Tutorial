c70a88e599d0fe04915ddc657ef4e1be5d957641
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-06-14 22:46:54 +0700

    Type Annotations

---
+++ projects/lesson6/lesson6.js
@@ -0,0 +1,6 @@
+let count = 1;
+count = "name";
+console.log(count);
+let names = ["thinh", "nam", "thanh"];
+names.push(24);
+console.log(names);
---
+++ projects/lesson6/lesson6.ts
@@ -0,0 +1,6 @@
+//TypeScript khuyến khích định nghĩa datatype (annotations)
+let count2: number = 1;
+console.log(count2);
+let names2: string[] = ["thinh", "nam", "thanh"];
+names2.push("24");
+console.log(names2);

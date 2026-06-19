f6463384a4e2ea3a20817dabd20f8097e5770ea5
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-06-19 08:27:43 +0700

    Array Type

---
+++ projects/lesson12/lesson12.ts
@@ -0,0 +1,8 @@
+let test: string[] = ["Thịnh"];
+test.push("Nam");
+console.log(test);
+
+//Lưu trữ mixed type
+let test2: (string | number)[] = ["Thịnh", 20];
+test2.push("Nam");
+console.log(test2);

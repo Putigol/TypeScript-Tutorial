63e630ad51c5b3ee9da0b2d67d34f176c8dfe11e
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-06-19 08:47:38 +0700

    Tuple Type

---
+++ projects/lesson13/lesson13.ts
@@ -0,0 +1,7 @@
+let skill: (string | number)[] = ["html", "24"];
+skill.push("css");
+console.log(skill);
+
+//Tuple : dataType/size/order
+let skill2: [string, number] = ["html", 24];
+console.log(skill2);

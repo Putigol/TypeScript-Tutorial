39aaa48a9c353444ff2f76cce87901bb0cb3a856
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-06-19 23:23:49 +0700

    switch-case

---
+++ projects/lesson21/lesson21.ts
@@ -0,0 +1,11 @@
+const age: number = 20;
+switch (age) {
+  case 23:
+    console.log("Đã đi làm");
+    break;
+  case 19:
+    console.log("Còn là sinh viên đại học");
+    break;
+  default:
+    console.log("Run default");
+}

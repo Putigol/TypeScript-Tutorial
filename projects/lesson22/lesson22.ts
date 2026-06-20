1cb8c7436666b084cd69442723b66f490a8017e1
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-06-20 15:41:13 +0700

    break

--- projects/lesson22/lesson22.ts
+++ projects/lesson22/lesson22.ts
@@ -1,8 +1,32 @@
-let counter2 = 0;
-do {
-  console.log(counter2);
-  if (counter2 % 2 === 1) {
+let products = [
+  { id: 1, name: "Laptop", price: 1000 },
+  { id: 2, name: "Phone", price: 500 },
+  { id: 3, name: "Tablet", price: 800 },
+];
+
+for (let i = 0; i < products.length; i++) {
+  if (products[i].price > 500) {
+    console.log(products[i].name);
     break;
+    console.log(products[i]);
   }
-  counter2++;
-} while (counter2 < 10);
+}
+
+let discount = 0;
+let product = products[1];
+switch (product.name) {
+  case "Laptop":
+    discount = 5;
+    break;
+  case "Phone":
+    discount = 10;
+    break;
+  case "Tablet":
+    discount = 15;
+    break;
+  default:
+    discount = 0;
+    break;
+}
+
+console.log(`You get a discount of ${discount}% for ${product.name}`);

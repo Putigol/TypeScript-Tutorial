c420fb77a527f11324cccdb37d762af86e11a7b1
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-06-20 15:53:26 +0700

    continue

--- projects/lesson22/lesson22.ts
+++ projects/lesson22/lesson22.ts
@@ -1,32 +1,20 @@
-let products = [
-  { id: 1, name: "Laptop", price: 1000 },
-  { id: 2, name: "Phone", price: 500 },
-  { id: 3, name: "Tablet", price: 800 },
-];
-
-for (let i = 0; i < products.length; i++) {
-  if (products[i].price > 500) {
-    console.log(products[i].name);
-    break;
-    console.log(products[i]);
-  }
+for (let index = 0; index < 9; index++) {
+  if (index % 2 === 0) continue;
+  console.log(index);
 }
 
-let discount = 0;
-let product = products[1];
-switch (product.name) {
-  case "Laptop":
-    discount = 5;
-    break;
-  case "Phone":
-    discount = 10;
-    break;
-  case "Tablet":
-    discount = 15;
-    break;
-  default:
-    discount = 0;
-    break;
+//while
+let index = -1;
+while (index < 9) {
+  index++;
+  if (index % 2 === 0) continue;
+  console.log(index);
 }
 
-console.log(`You get a discount of ${discount}% for ${product.name}`);
+//do while
+let index2 = -1;
+do {
+  index2++;
+  if (index2 % 2 === 0) continue;
+  console.log(index2);
+} while (index2 < 9);

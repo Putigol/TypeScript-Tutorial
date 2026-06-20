eed8a4ec7dbd9bbd0a911e02296a0d2ac8a105f6
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-06-20 16:16:24 +0700

    funtion/arrow function

---
+++ projects/lesson23/lesson23.ts
@@ -0,0 +1,12 @@
+function sum(soA: number, soB: number): number {
+  return soA + soB;
+}
+
+console.log(sum(1, 2));
+
+//arrow function
+const sum2 = (soA: number, soB: number): number => {
+  return soA + soB;
+};
+
+console.log(sum2(1, 23));

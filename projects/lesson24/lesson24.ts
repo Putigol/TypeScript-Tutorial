5b1a82fd6e51af2022d4a0f629d00a4ec0371caa
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-06-20 23:01:10 +0700

    Rest Parameters

--- projects/lesson24/lesson24.ts
+++ projects/lesson24/lesson24.ts
@@ -1,10 +1,33 @@
-//Default Parameters
-const sum24 = (soA: number, soB: boolean = false, soC: number) => {
-  if (soB === false) {
-    return soA + soC;
-  }
-  if (soB) {
-    return soA - soC;
-  }
-};
-console.log(sum24(1, true, 2));
+//Rest Parameter
+function getTotal(...numbers: number[]): number {
+  let total = 0;
+  numbers.forEach((number) => {
+    total += number;
+  });
+  return total;
+}
+
+console.log(getTotal());
+
+//Để mảng ở tham số cuối cùng
+function getMutiply(base: number, ...m: number[]) {
+  return m.map((x) => base * x);
+}
+
+function getMinus(base: number, ...m: number[]) {
+  let result = m.map((x) => {
+    console.log("x= ", x);
+    return base - x;
+  });
+  return m.map((x) => base - x);
+}
+
+console.log(getMutiply(2, 1, 2, 3));
+console.log(getMinus(2, 1, 2, 3));
+
+//String
+function getFullName(action: string, ...names: string[]) {
+  return action + " " + names.join(", ") + "!";
+}
+
+console.log(getFullName("hello", "nam", "thanh"));

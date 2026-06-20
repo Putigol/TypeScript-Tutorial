07bbdf254921fc17bfbf5f1933cc66046b3f149c
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-06-20 10:03:07 +0700

    while

--- projects/lesson22/lesson22.ts
+++ projects/lesson22/lesson22.ts
@@ -1,3 +1,12 @@
-for (let i = 0; i < 10; i++) {
-  console.log(i);
+let counter = 0;
+while (counter < 10) {
+  console.log(counter);
+  counter += 1;
+}
+
+let counter2 = 3;
+while (counter2 < 10) {
+  console.log(counter2);
+  if (counter2 % 2 === 0) break;
+  counter2 += 1;
 }

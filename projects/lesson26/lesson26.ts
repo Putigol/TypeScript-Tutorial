3037c3e7632c441443dc9c0ccbf884c079ae2ee2
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-06-21 17:02:21 +0700

    readonly

--- projects/lesson26/lesson26.ts
+++ projects/lesson26/lesson26.ts
@@ -1,21 +1,28 @@
 //Access Modifier
 
 class Employee {
+  readonly birthDate: string;
   private empCode: string;
   empName: string;
-  constructor(empCode: string, empName: string) {
+  constructor(birthDate: string, empCode: string, empName: string) {
     this.empCode = empCode;
     this.empName = empName;
+    this.birthDate = birthDate;
   }
 }
 
 class SalesEmployee extends Employee {
   private department: string;
-  constructor(empCode: string, empName: string, department: string) {
-    super(empCode, empName);
+  constructor(
+    birthDate: string,
+    empCode: string,
+    empName: string,
+    department: string,
+  ) {
+    super(birthDate, empCode, empName);
     this.department = department;
   }
 }
 
-let emp = new SalesEmployee("123", "Thinh", "IT");
+let emp = new SalesEmployee("10/10/2000", "123", "Thinh", "IT");
 console.log(emp);

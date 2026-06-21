4ada8397a33a5b1b7b9d7ee40ab67c8329e75e46
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-06-21 09:54:28 +0700

    Introduction to Class

---
+++ projects/lesson25/lesson25.ts
@@ -0,0 +1,18 @@
+class Person {
+  ssn: string;
+  firstName: string;
+  lastName: string;
+
+  constructor(ssn: string, firstName: string, lastName: string) {
+    this.ssn = ssn;
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  getFullName(): string {
+    return this.firstName + " " + this.lastName;
+  }
+}
+
+const person = new Person("123456789", "John", "Doe");
+console.log(person.getFullName());

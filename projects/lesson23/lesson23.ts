877ea15db35603b046d05f383c715f9478d0edb0
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-06-21 09:38:39 +0700

    Function Overloading

--- projects/lesson23/lesson23.ts
+++ projects/lesson23/lesson23.ts
@@ -1,12 +1,30 @@
-function sum(soA: number, soB: number): number {
-  return soA + soB;
+// Function Overloading
+function addNew(a: number, b: number): number;
+function addNew(a: string, b: string): string;
+function addNew(a: any, b: any) {
+  return a + b;
 }
 
-console.log(sum(1, 2));
+console.log(addNew(1, 2));
+console.log(addNew("1", "2"));
 
-//arrow function
-const sum2 = (soA: number, soB: number): number => {
-  return soA + soB;
-};
+//For class
+class Counter {
+  private current: number = 0;
+  count(): number;
+  count(target: number): number[];
+  count(target?: number): number | number[] {
+    if (target) {
+      let values: number[] = [];
+      for (let start = this.current; start <= target; start++) {
+        values.push(start);
+      }
+      return values;
+    }
+    return ++this.current;
+  }
+}
 
-console.log(sum2(1, 23));
+let counter = new Counter();
+console.log(counter.count());
+console.log(counter.count(10));
